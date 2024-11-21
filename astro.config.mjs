687e64ec6001acb7a07a1597dfc0ddd1e29662ff
Santiago Escurra <escurrasantiago1@gmail.com>
2024-11-21 16:26:06 -0500

    markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import markdown from '@astro/markdown';
 // https://astro.build/config
-export default defineConfig({});
+
+export default defineConfig({
+  integrations: [markdown()],
+});
