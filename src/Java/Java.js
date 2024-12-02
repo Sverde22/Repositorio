61346b93883a76b33d31d8f28c05ff7d8b400611
Santiago Escurra <escurrasantiago1@gmail.com>
2024-12-01 19:48:13 -0500

    dgds

--- src/Java/Java.js
+++ src/Java/Java.js
@@ -1,10 +1,22 @@
- 
-const button1 = document.getElementById("button1");
-const addsong =document.getElementsByClassName("addsong");
-const awollist =document.getElementsByClassName("awollist");
 
-button1.addEventListener("click", () => {
-const addnewsong= addsong.value.trim();
 
-if (addnewsong) { const add1 = document.createElement("li"); add1.textContent = addnewsong; awollist.appendChild(add1); addsong.value="";} else { alert("");}
-});
+const button1 = document.getElementById ("button1");
+const addsong = document.getElementById ("addsong");
+const awollist = document.getElementById ("awollist");
+
+button1.addEventListener('click', () => {
+    const button2= addsong.value.trim(); 
+    if (button2) {
+     
+      const button3 = document.createElement('li');
+      button3.textContent = button2;
+  
+   
+      awollist.appendChild(button3);
+  
+     
+      addsong.value = '';
+    } else {
+      alert('');
+    }
+  });
